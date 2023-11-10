ca67da7647a0dac2b97b54dcf44705a2e9fda837
RY0908 <rayanrayman901@gmail.com>
2023-11-10 19:29:29 +0100

    added services.jsx

---
+++ Frontend1/src/Components/Services.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Services() {
+  return (
+    <div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+}
+
+export default Services;
